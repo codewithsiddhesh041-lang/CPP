03b037ce68a638d3bf50933691575d3b71c0cdb2
Siddhesh  Vishnu Kumbhar <codewithsiddhesh041@gmail.com>
2026-08-03 23:57:59 +0530

    Add files via upload

---
+++ c_11.cpp
@@ -0,0 +1,17 @@
+//  Function in c++
+
+# include<iostream>
+using namespace std;
+
+int add(int a,int b)
+{
+    return a+b;
+}
+
+
+int main()
+{
+    int sum=add(10,5);
+    cout<< sum ;
+    return 0;
+}
