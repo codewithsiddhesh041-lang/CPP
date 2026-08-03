fc1c267036d5044bc1571d23dd2e45d1f19f2f2c
Siddhesh  Vishnu Kumbhar <codewithsiddhesh041@gmail.com>
2026-08-03 23:57:41 +0530

    Add files via upload

---
+++ c_10.cpp
@@ -0,0 +1,17 @@
+//  Do while loop
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=1;
+    do
+    {
+        cout<<i<<"\n";
+        i++;
+    } while(i<=10);
+
+    return 0;
+}
