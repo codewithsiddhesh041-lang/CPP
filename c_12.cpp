4e83196b226ebb504849007a235f2a0051cf6f36
Siddhesh  Vishnu Kumbhar <codewithsiddhesh041@gmail.com>
2026-08-03 23:59:09 +0530

    Add files via upload

---
+++ c_12.cpp
@@ -0,0 +1,17 @@
+// function using return_type is void
+
+#include <iostream>
+using namespace std;
+
+// Function using return type void
+void sid()
+{
+    cout << "Hii Siddhesh";
+}
+
+int main()
+{
+    sid();    // Function call
+
+    return 0;
+}
