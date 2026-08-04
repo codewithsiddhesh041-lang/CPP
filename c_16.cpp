41c57dd007de0662dcef186b50baec8eb3f0941c
Siddhesh  Vishnu Kumbhar <codewithsiddhesh041@gmail.com>
2026-08-04 23:55:06 +0530

    Add files via upload

---
+++ c_16.cpp
@@ -0,0 +1,17 @@
+// changing value using pointer  
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+
+    int *p = &a;
+
+    *p = 50;
+
+    cout << a;
+
+    return 0;
+}
