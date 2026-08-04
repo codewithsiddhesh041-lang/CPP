5ca314ee74974abab00ee31fecb6606699b6f2e1
Siddhesh  Vishnu Kumbhar <codewithsiddhesh041@gmail.com>
2026-08-04 23:53:36 +0530

    Add files via upload

---
+++ c_13.cpp
@@ -0,0 +1,19 @@
+// find square using function
+
+
+#include<iostream>
+
+using namespace std;
+
+
+int square(int n)
+{
+    return n*n;
+}
+
+int main()
+{
+    int s=square(5);
+    cout<<s;
+    return 0;
+}
