c76afe964c173f68f24a7dc5301f6c9897e0ad35
Siddhesh  Vishnu Kumbhar <codewithsiddhesh041@gmail.com>
2026-08-04 23:54:35 +0530

    Add files via upload

---
+++ c_15.cpp
@@ -0,0 +1,19 @@
+//  pointer in c++
+
+
+# include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=10;
+    int * p=&a;
+
+    cout<<a<<endl;
+    cout<<&a<<endl;
+    cout<<p<<endl;
+    cout<<&p<<endl;
+    cout<<*p;
+
+}
