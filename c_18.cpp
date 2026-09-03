dd2ed774309baa70629d65e7091917474c3b6f84
Siddhesh  Vishnu Kumbhar <codewithsiddhesh041@gmail.com>
2026-09-04 00:51:46 +0530

    Add files via upload

---
+++ c_18.cpp
@@ -0,0 +1,16 @@
+// static memory allocation
+
+#include<iostream>
+using namespace std;
+
+void fun1()
+{
+    int arr[5]={10,20,30,40,50};
+    cout<<arr[2];
+
+}
+int main()
+{
+    fun1();
+    return 0;
+}
